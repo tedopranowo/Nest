fcb812e82262f0a6f012736a8b666021cd42a921
Tedo Pranowo <tedokdr@yahoo.com>
2019-01-25 21:16:58 +0700

    Add bird code and Border images

--- Assets/Nest/Scripts/Bird.cs
+++ Assets/Nest/Scripts/Bird.cs
@@ -1,12 +1,13 @@
 ﻿//-------------------------------------------------------------------------------------------------
 // Bird.cs
-// - Controllable by the player with user input
-//
+// - Character controllable by the player's input
 //-------------------------------------------------------------------------------------------------
 using UnityEngine;
 
 public class Bird : MonoBehaviour {
-    [SerializeField] private int 
+    [SerializeField] private float m_speed;
+    [SerializeField] private float m_rotationSpeed;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -14,6 +15,23 @@ public class Bird : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        HandleRotation();
+
+        HandleForwardMovement();
 	}
+
+    private void HandleRotation()
+    {
+        //Get horziontal input
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        //Rotate the character 
+        transform.Rotate(horizontalInput * Vector3.back * m_rotationSpeed * Time.deltaTime);
+    }
+
+    private void HandleForwardMovement()
+    {
+        //Move the bird forward
+        transform.position += transform.up * Time.deltaTime * m_speed;
+    }
 }
